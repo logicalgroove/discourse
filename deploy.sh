d64f9d10106a02fccabf6e79d791e486d603015a
atanych <atanych@gmail.com>
2017-02-27 14:43:41 +0300

    add migrate to deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo "$(tput setaf 2) Fetching data from git$(tput sgr 0)"
 git pull origin master
 echo "$(tput setaf 2) Installing gems$(tput sgr 0)"
 bundle install
-#bundle exec rake db:migrate
+bundle exec rake db:migrate
 echo "$(tput setaf 2) Assets precompile$(tput sgr 0)"
 bundle exec rake assets:precompile
 echo "$(tput setaf 2) Restarting unicorn server$(tput sgr 0)"
