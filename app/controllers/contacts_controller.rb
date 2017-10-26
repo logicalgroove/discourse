52f722fc58a53d70ca628a27a43229856bf4a9a7
Logical Groove <info@logicalgroove.com>
2017-10-26 09:17:55 +0000

    Update filter to action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,11 @@
 require_dependency 'rate_limiter'
 
 class ContactsController < ApplicationController
-  skip_before_filter :redirect_to_login_if_required
-  skip_before_filter :check_xhr
-  skip_after_filter  :perform_refresh_session
-  skip_before_filter :verify_authenticity_token
-  before_filter :ensure_logged_in, only: [:live_post_counts]
+  skip_before_action :redirect_to_login_if_required
+  skip_before_action :check_xhr
+  skip_after_action  :perform_refresh_session
+  skip_before_action :verify_authenticity_token
+  before_action :ensure_logged_in, only: [:live_post_counts]
 
   def index
     respond_to do |format|
